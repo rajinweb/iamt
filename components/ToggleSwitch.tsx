29ba098d098bf455e45575b406b71bedb5880d4f
Rajesh <rajinweb@gmail.com>
2025-04-05 09:41:00 +0530

    toggle changes

--- components/ToggleSwitch.tsx
+++ components/ToggleSwitch.tsx
@@ -21,13 +21,13 @@ const ToggleSwitch: React.FC<ToggleSwitchProps> =React.memo( ({ checked, onChang
         disabled={disabled}
       />
       {/* Track */}
-      <div className="absolute top-0 left-0 w-full h-full bg-gray-400 rounded-full peer-checked:bg-blue-500 transition-all"></div>
+      <div className="absolute w-full mt-1 h-4 bg-gray-400 rounded-full peer-checked:bg-blue-300 transform transition-all"></div>
       {/* Thumb */}
-      <div className="absolute top-1 left-1 w-4 h-4 bg-white dark:bg-blue-50 rounded-full shadow-md transition-all peer-checked:translate-x-6">
+      <div className="absolute top-0 left-0 w-6 h-6 bg-gray-100 border-gray-400 border peer-checked:border-0 peer-checked:bg-[#114295] rounded-full shadow-md transition-all peer-checked:translate-x-6  ">
          {iconEnable &&
           (isChecked ? <Check size={16} strokeWidth={4} className="scale-80" /> : <X size={16} strokeWidth={4} className="scale-80"/>)
         }
-      </div>
+      </div> 
     </label>
   );
 });
